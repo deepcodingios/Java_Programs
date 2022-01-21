95b6284f63976d8fc420ca34f9fedd3544482bc6
PradeepReddy <deepu462@gmail.com>
2022-01-21 23:30:49 +0530

    Adding Palindrome Method

--- src/Assignments/EvenOrOdd.java
+++ src/Assignments/EvenOrOdd.java
@@ -1,15 +1,20 @@
 package Assignments;
 
-import com.sun.source.tree.AssignmentTree;
+//import com.sun.source.tree.AssignmentTree;
 
 public class EvenOrOdd {
     public static void main(String[] args) {
         EvenOrOdd obj = new EvenOrOdd();
         obj.isOddOrEven();
+        obj.isPalindrome();
 
     }
 
     void isOddOrEven(){
         System.out.println("ODD");
+    }
+
+    void isPalindrome(){
+        System.out.println("Palindrome Number");
     }
 }
