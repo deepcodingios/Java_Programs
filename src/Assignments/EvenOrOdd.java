77f185a48d9079e4ea7ccf3c07ae15c990abb6f6
PradeepReddy <deepu462@gmail.com>
2022-01-22 22:39:34 +0530

    Solution for EvenODD Number Program

--- src/Assignments/EvenOrOdd.java
+++ src/Assignments/EvenOrOdd.java
@@ -2,6 +2,8 @@ package Assignments;
 
 //import com.sun.source.tree.AssignmentTree;
 
+import java.util.Scanner;
+
 public class EvenOrOdd {
     public static void main(String[] args) {
         EvenOrOdd obj = new EvenOrOdd();
@@ -11,7 +13,16 @@ public class EvenOrOdd {
     }
 
     void isOddOrEven(){
-        System.out.println("ODD");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int inputNumber = input.nextInt();
+
+        if (inputNumber % 2 == 0){
+            System.out.println("This is an Even Number");
+        }else {
+            System.out.println("This is an ODD Number");
+        }
     }
 
     void isPalindrome(){
