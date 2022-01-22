52fba8addb0e1e66a2eb2f258961e4ce8c683674
PradeepReddy <deepu462@gmail.com>
2022-01-22 22:59:31 +0530

    Print Welcome Message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+        // write your code here
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String welcomeMessage = input.nextLine();
+        System.out.println("Hello " + welcomeMessage);
+
     }
 }
