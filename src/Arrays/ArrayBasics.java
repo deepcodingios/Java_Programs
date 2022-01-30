f042a9e91728496ef26dcbace58343001b0a5805
PradeepReddy <deepu462@gmail.com>
2022-01-30 13:17:47 +0530

    Basics of Array

--- src/Arrays/ArrayBasics.java
+++ src/Arrays/ArrayBasics.java
@@ -1,5 +1,25 @@
 package Arrays;
 
 public class ArrayBasics {
+    public static void main(String[] args) {
 
+        //Syntax
+        //datatype[] variable_name = new datatype[size];
+        //Store 5 roll numbers:
+        int[] array = new int[5];
+        //or directly
+
+        int[] rollNumbers = {1,2,3,4,5};
+
+        //Integer Array
+        int[] ros;//declaration of array. ros is getting defined in the stack
+        ros = new int[5];//Initialisation: Object here is being created in the memory(heap)
+        System.out.println(rollNumbers[2]);
+
+        //String Array
+        String[] names = new String[4];
+//        String[] names = {};
+        System.out.println(names[1]);
+
+    }
 }
