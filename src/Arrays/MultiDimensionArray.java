f5ea326574552f5039da7ec3da038f209e063288
PradeepReddy <deepu462@gmail.com>
2022-01-30 22:02:51 +0530

    Example of Multi Dimensional Array

---
+++ src/Arrays/MultiDimensionArray.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class MultiDimensionArray {
+    public static void main(String[] args) {
+        /*
+        1 2 3
+        4 5 6
+        7 8 9
+        */
+
+        //It's important to define the number of rows for a 2D Dimensional Array. It's Mandatory.
+        int[][] multiDimentionalArray = new int[3][3];
+
+        int [] [] array2D = {
+                {1,2,3},
+                {4,5},
+                {7,8,9}
+        };
+    }
+}
