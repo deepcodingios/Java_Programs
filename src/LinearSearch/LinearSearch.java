ce6b43464dd05c07c8cca7e64850bc770b59f187
PradeepReddy <deepu462@gmail.com>
2022-01-31 23:03:40 +0530

    Example of Linear Search Algorithm

---
+++ src/LinearSearch/LinearSearch.java
@@ -0,0 +1,28 @@
+package LinearSearch;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+
+        int[] array = {34,56,78,94,64,33};
+        int target = 323;
+        int answer = linearSearch(array,target);
+        System.out.println(answer);
+    }
+
+    //Search in the Array: Return the index if item found. Return -1 is item not found
+    static int linearSearch(int[] array, int integer){
+        if (array.length == 0){
+            return -1;
+        }
+
+        for (int index = 0; index < array.length; index++){
+            //Check for the element
+            int element = array[index];
+            if (element == integer){
+                return index;
+            }
+        }
+
+        return -1;
+    }
+}
