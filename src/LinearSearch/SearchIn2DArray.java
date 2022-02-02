d8b6fbfac834d692109cf9d447d0b05759c6396c
PradeepReddy <deepu462@gmail.com>
2022-02-02 07:40:41 +0530

    Solution to search a Nnumber in a 2D Array

---
+++ src/LinearSearch/SearchIn2DArray.java
@@ -0,0 +1,32 @@
+package LinearSearch;
+
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int[][] array = new int[][]{
+                {34, 5, 6},
+                {56, 7, 8},
+                {45, 67, 90},
+                {18,12}
+        };
+        int target = 12;
+
+        int[] answer = searchArray(array,target);
+
+        System.out.println(Arrays.toString(answer));
+
+    }
+
+    static int[] searchArray(int[][] array, int target){
+        for (int row = 0; row < array.length; row++){
+            for (int col = 0; col < array[row].length; col++){
+                if (array[row][col] == target){
+                    return new int[] {row,col};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+}
