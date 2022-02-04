9305f4cdb50643cd4676cfa49ad787a920de2131
PradeepReddy <deepu462@gmail.com>
2022-02-04 22:31:09 +0530

    Solution to find the index of a number in an Array using Binary Search Algorithm

--- src/BinaySearch/FindNumber.java
+++ src/BinaySearch/FindNumber.java
@@ -1,7 +1,36 @@
 package BinaySearch;
 
+import java.util.Arrays;
+
 public class FindNumber {
     public static void main(String[] args) {
 
+        int[] array = {1,2,3,4,5,6,77,88,99,102,105,115,167};
+        int answer = binarySearch(array,105);
+        System.out.println(answer);
+
+    }
+
+    static int binarySearch(int[] array, int target){
+
+        int startIndex = 0;
+        int endIndex = array.length -1;
+
+        while(startIndex <= endIndex){
+            //Find Middle Element
+            int middle = (startIndex+endIndex)/2;
+            if (target == array[middle]){
+                return middle;
+            }else {
+                if (target > array[middle]){
+                    startIndex = middle+1;
+                    System.out.println("Start Index Incremented");
+                }else{
+                    endIndex = middle-1;
+                    System.out.println("End Index Decremented");
+                }
+            }
+        }
+        return -1;
     }
 }
