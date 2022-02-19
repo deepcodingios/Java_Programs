7942991f82667ca63ef8868ac310d333daecdcb8
PradeepReddy <deepu462@gmail.com>
2022-02-19 20:54:56 +0530

    Basic:- Solution to print Multiplication Table

---
+++ src/Scaler/Assignments/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package Scaler.Assignments;
+import java.util.*;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            int result = number * i;
+            System.out.println(number + " * " + i + " = " + result);
+        }
+    }
+}
