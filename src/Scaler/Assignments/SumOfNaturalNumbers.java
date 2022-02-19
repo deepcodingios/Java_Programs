78bfb4fea59837642e1413acfd73daa64b670153
PradeepReddy <deepu462@gmail.com>
2022-02-19 12:31:49 +0530

    Basic:- Solution to find Sum of Natural Numbers in Ascending Order

---
+++ src/Scaler/Assignments/SumOfNaturalNumbers.java
@@ -0,0 +1,13 @@
+package Scaler.Assignments;
+import java.util.*;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        for(int i = 1; i <= number; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
