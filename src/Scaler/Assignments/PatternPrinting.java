51d037c159e862366e3f8caef9264734cb48b3f0
PradeepReddy <deepu462@gmail.com>
2022-02-25 10:29:10 +0530

    Basic:- Solution to Pattern Printing 1

--- src/Scaler/Assignments/PatternPrinting.java
+++ src/Scaler/Assignments/PatternPrinting.java
@@ -19,14 +19,67 @@ public class PatternPrinting {
 //            System.out.println("");
 //        }
 
-        for (int i=1; i < number; i++) {
-            System.out.print("*");
-            int spaces = number-i;
+        //------------------------------------------------------------------------
+        /*
+        *___*
+        *__*
+        *_*
 
-            for (int j = 1; j <= spaces; j++) {
-                System.out.print(" ");
+        */
+//        for (int i=1; i < number; i++) {
+//            System.out.print("*");
+//            int spaces = number-i;
+//
+//            for (int j = 1; j <= spaces; j++) {
+//                System.out.print(" ");
+//            }
+//            System.out.println("*");
+//        }
+
+        //------------------------------------------------------------------------
+
+        //------------------------------------------------------------------------
+        /*
+
+        ______*
+        _____**
+        ____***
+        ___****
+        __*****
+        _******
+        *******
+
+         */
+
+//        for (int rows = 1; rows <= number; rows++) {
+////            System.out.print(number);
+//            int spaces = number-1;
+//            for (int j = 1; j <= spaces; j++) {
+//                System.out.print("_");
+//            }
+//            int stars = rows;
+//            for (int i = 1; i <= stars; i++) {
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+        //------------------------------------------------------------------------
+
+        for (int i = 1; i <= number; i++) {
+
+            /*Spaces*/
+            for (int spaces = 1; spaces <= i-1; spaces++) {
+                System.out.print("_");
             }
-            System.out.println("*");
+            /*Stars*/
+            for (int stars = 1; stars <= number-i; stars++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
         }
+
+        //------------------------------------------------------------------------
+
     }
 }
