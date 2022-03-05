2fdbe918bcb6c71d7b8e02b581706e84821f9582
PradeepReddy <deepu462@gmail.com>
2022-03-05 10:41:17 +0530

    Arrays:- Solution to find the NewsPaper Reading Problem

--- src/Scaler/Assignments/ReadingNewsPaper.java
+++ src/Scaler/Assignments/ReadingNewsPaper.java
@@ -1,49 +1,33 @@
-//package Scaler.Assignments;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//public class ReadingNewsPaper {
-//    public static void main(String[] args) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        ArrayList<String> gfg = new ArrayList<String>() {
-//            {
-//                gfg.a
-//
-//                gfg.add();add(String.valueOf(100));
-//                add(String.valueOf(100));
-//                add(String.valueOf(100));
-//                add(String.valueOf(100));
-//                add(String.valueOf(100));
-//                add(String.valueOf(100));
-//            }
-//        };
-//        solve(scanner.nextInt(),gfg);
-//
-//
-//    }
-//
-//    static int solve(int A, ArrayList<Integer> B) {
-//
-//        int remainingLines = A;
-//
-//        int i = 0;
-//
-//        while (remainingLines > 0){
-//
-//        }
-//
-//        for(int i = 0; i < B.size(); i++){
-//
-//            if(remainingLines > 0){
-//                remainingLines = remainingLines - B.get(i);
-//            }else{
-//                return i;
-//            }
-//        }
-//        return 1;
-//    }
-//}
+package Scaler.Assignments;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReadingNewsPaper {
+    public static void main(String[] args) {
+
+        int totalLines = 1000;
+        int [] array = {100, 100, 100, 100, 100, 100};
+
+        int weekNumber = solve(totalLines,array);
+        System.out.println(weekNumber);
+    }
+
+    static int solve(int A, int [] arr) {
+
+        int remainingLines = A;
+
+        int i = 0;
+
+        while (remainingLines > 0){
+            if (i == arr.length){
+                i = 0;
+            }
+            remainingLines = remainingLines - arr[i];
+            i++;
+        }
+
+        return i;
+    }
+}
