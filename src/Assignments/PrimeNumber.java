9aa14886118df50fcd791319fbf8e9aca7374dd5
PradeepReddy <deepu462@gmail.com>
2022-03-06 23:11:41 +0530

    Basic:- Solution to find if the number is Prime using For Loop

--- src/Assignments/PrimeNumber.java
+++ src/Assignments/PrimeNumber.java
@@ -8,7 +8,31 @@ public class PrimeNumber {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the Number:");
         int n = input.nextInt();
-        System.out.println(isPrime(n));
+        System.out.println(isPrimeNumber(n));
+    }
+
+    static boolean isPrimeNumber(int number) {
+        int count = 0;
+
+        for(int i = 1; i <= number; i++){
+            System.out.println("Count is " + count + " and i value is " + i);
+            if (number % i == 0){
+                System.out.println("i value is " + i);
+                count ++;
+            }
+
+            if (count >= 3){
+                System.out.println("Exited the loop with count: " + count);
+                break;
+            }
+        }
+        if(count == 2){
+            System.out.println("YES");
+            return true;
+        }else{
+            System.out.println("NO");
+            return false;
+        }
     }
 
     static boolean isPrime(int n){
