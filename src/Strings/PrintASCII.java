a1447975eb948c6c6a2c1c4765573f9bcf793f05
PradeepReddy <deepu462@gmail.com>
2022-03-15 08:44:07 +0530

    Solution to print ASCCI characters

---
+++ src/Strings/PrintASCII.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class PrintASCII {
+    public static void main(String[] args) {
+        for (int i = 33; i < 128; i++) {
+            char ch = (char)i;
+            System.out.println(i +" "+ch);
+
+        }
+    }
+}
