811fa1cdd392ea3725ff4df73d8d586a1cb2d366
PradeepReddy <deepu462@gmail.com>
2022-03-19 07:55:59 +0530

    Solution to find square root of a number

---
+++ src/Basics/SquareRootOfNumber.java
@@ -0,0 +1,28 @@
+package Basics;
+
+public class SquareRootOfNumber {
+    public static void main(String[] args) {
+
+        int result = findSquareRootOfNumber(11);
+        System.out.println(result);
+    }
+
+    static int findSquareRootOfNumber(int number){
+
+        // Base cases
+        if (number == 0 || number == 1)
+            return number;
+
+        // Starting from 1, try all numbers until
+        // i*i is greater than or equal to x.
+        int i = 1, result = 1;
+
+        while (result <= number) {
+            i++;
+            result = i * i;
+            System.out.println(i + " " + result);
+        }
+        return i - 1;
+
+    }
+}
