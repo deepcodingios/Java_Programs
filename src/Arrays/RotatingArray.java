765f9499fa1c1f6d3f7fb3c214854c7f70f10150
PradeepReddy <deepu462@gmail.com>
2022-03-22 22:11:28 +0530

    Fix for resolving Time Complexity in Rotating Array

--- src/Arrays/RotatingArray.java
+++ src/Arrays/RotatingArray.java
@@ -7,12 +7,20 @@ public class RotatingArray {
 
         int [] array = {1,2,3,4,5};
 
-        rotateArray(array,2);
+        rotateArray(array,97);
     }
 
     static void rotateArray(int [] array, int rotation){
 
         int lengthOfArray = array.length;
+
+        if(rotation % lengthOfArray == 0){
+            rotation = lengthOfArray;
+        }else{
+            rotation = rotation%lengthOfArray;
+        }
+
+        System.out.println(rotation);
 
         for (int j = 1; j <= rotation; j++) {
 
@@ -22,6 +30,7 @@ public class RotatingArray {
                 array[i+1] = array[i];
             }
             array[0] = temp;
+//            System.out.println(Arrays.toString(array));
         }
 
         System.out.println(Arrays.toString(array));
