ea9b352acb162e15794fcee2084bb021571e0186
PradeepReddy <deepu462@gmail.com>
2022-05-25 10:24:34 +0530

    Solution to write a Program to check if two trees are identical

--- src/Trees/Trees.java
+++ src/Trees/Trees.java
@@ -50,4 +50,15 @@ public class Trees {
 
         return list;
     }
+
+    static int isSameTree(TreeNode A, TreeNode B) {
+
+        if (A == null && B == null){return 1;}
+        if (A == null || B == null){return 0;}
+
+        if ((A.val == B.val) && (isSameTree(A.left,B.left) == 1) && (isSameTree(A.right,B.right) == 1)){
+            return 1;
+        }
+        return 0;
+    }
 }
