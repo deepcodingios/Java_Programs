ca8f96fc2e0c076cf23457924be21ed2f6c55499
PradeepReddy <deepu462@gmail.com>
2022-05-27 19:47:03 +0530

    Solution to write a Program to check the equilibrium index of an array

--- src/Arrays/EquilibriumIndex.java
+++ src/Arrays/EquilibriumIndex.java
@@ -5,7 +5,33 @@ import java.util.ArrayList;
 public class EquilibriumIndex {
     public static void main(String[] args) {
 
-        findEquilibriumIndex();
+        ArrayList<Integer> arrayList = new ArrayList<>(100);
+
+        arrayList.add(-7);
+        arrayList.add(1);
+        arrayList.add(5);
+        arrayList.add(2);
+        arrayList.add(-4);
+        arrayList.add(3);
+        arrayList.add(0);
+//        arrayList.add(-3);
+//        arrayList.add(6);
+//        arrayList.add(2);
+//        arrayList.add(4);
+//        arrayList.add(5);
+//        arrayList.add(-1);
+//        arrayList.add(2);
+//        arrayList.add(-3);
+//        arrayList.add(6);
+//        arrayList.add(2);
+//        arrayList.add(4);
+//        arrayList.add(5);
+//        arrayList.add(-1);
+//        arrayList.add(2);
+
+        int index = findEquilibriumIndex(arrayList);
+
+        System.out.println("Equilibrium Index is " + index);
     }
 
     static int getPrefixSumOfAnIndex(int index, ArrayList<Integer> arrayList){
@@ -21,19 +47,41 @@ public class EquilibriumIndex {
         return prefixSum;
     }
 
-    static void findEquilibriumIndex() {
+    static int findEquilibriumIndex(ArrayList<Integer> arrayList) {
 
-        ArrayList<Integer> arrayList = new ArrayList<>(10);
+        ArrayList<Integer> prefixArray = getPrefixSumArrayOfAnArray(arrayList);         
 
-        arrayList.add(-3);
-        arrayList.add(6);
-        arrayList.add(2);
-        arrayList.add(4);
-        arrayList.add(5);
-        arrayList.add(-1);
-        arrayList.add(2);
+        System.out.println(prefixArray);
 
-        int prefixSum = getPrefixSumOfAnIndex(6,arrayList);
-        System.out.println("Prefix Sum is " + prefixSum);
+        int length = prefixArray.size()-1;
+
+        for (int i = 0; i < length; i++) {
+
+            int left = 0;
+            if (i == 0){
+                left = 0;
+            }else{
+                left = prefixArray.get(i -1);
+            }
+            int right = prefixArray.get(length) - prefixArray.get(i);
+            if (left == right){
+                return i;
+            }
+        }
+
+        return -1;
     }
+
+    static ArrayList<Integer> getPrefixSumArrayOfAnArray(ArrayList<Integer> list){
+
+        ArrayList<Integer> prefixArrayList = new ArrayList<>();
+
+        for (int i = 0; i < list.size()-1; i++) {
+
+            prefixArrayList.add(getPrefixSumOfAnIndex(i,list));
+        }
+        return prefixArrayList;
+    }
+
+
 }
