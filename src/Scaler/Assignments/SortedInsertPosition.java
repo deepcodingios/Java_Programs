d76b4220af921bf1131cc73f0c7f0476fa68e2d9
Pradeep_Reddy_Kypa <deepu462@gmail.com>
2023-01-16 17:27:57 +0530

    Solution to find Index of Search Element

---
+++ src/Scaler/Assignments/SortedInsertPosition.java
@@ -0,0 +1,37 @@
+package Scaler.Assignments;
+
+public class SortedInsertPosition {
+
+    public static void main(String[] args) {
+        
+        int[] array = {4,6,8,10,12};
+
+        int index = searchInsert(array, 5);
+
+        System.out.println(index);
+
+    }
+
+    static int searchInsert(int[] A, int target) {
+
+        // int outputIndex;
+
+        int startIndex = 0;
+        int endIndex = A.length-1;
+
+        while (startIndex <= endIndex) {
+
+            int middleIndex = startIndex + (endIndex - startIndex)/2;
+
+            if(target < A[middleIndex]){
+                endIndex = middleIndex-1;
+            }
+            else if(target > A[middleIndex]){
+                startIndex = middleIndex + 1;
+            }else{
+                return middleIndex;
+            }                        
+        }
+        return startIndex;
+    }
+}
