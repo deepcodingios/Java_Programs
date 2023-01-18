d5a7090dd8882d990d1985a7957caf09ab336873
Pradeep_Reddy_Kypa <deepu462@gmail.com>
2023-01-18 11:30:09 +0530

    Solution to find Max Sub Array Sum for a particular length

--- src/Arrays/MaxSubArraySum.java
+++ src/Arrays/MaxSubArraySum.java
@@ -2,7 +2,34 @@ package Arrays;
 
 public class MaxSubArraySum {
     public static void main(String[] args) {
-        
+
+        int [] array = {4,6,7,3,5,7,9,5,6,8,9};
+
+        int lengthOfSubArray = 5;
+
+        for(int i = 0; i<array.length; i++){
+
+            if(i+lengthOfSubArray-1 < array.length){
+
+                int answer = findSubArraySum(array, i, i+lengthOfSubArray-1);
+
+                System.out.println("Total Sum is " + answer);
+            }           
+        }
+    }
+
+    static int findSubArraySum(int [] subArray, int start, int end){
+
+        int sum = 0;
+
+        for(int i = start; i <= end; i ++){
+
+            sum = sum + subArray[i];
+            
+            System.out.println("The sum of SubArray is " + sum);
+        }        
+
+        return sum;
     }
     
     static void MaxSubArraySumForLength(int [] A, int length){
