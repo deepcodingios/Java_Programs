ad0ee9bf6d7ed8e0ea3e9c1bf8adb14a3e376c4a
Pradeep_Reddy_Kypa <deepu462@gmail.com>
2023-03-14 22:35:36 +0530

    Solution to sort array using merge sort

--- src/Sorting/SortingProblems.java
+++ src/Sorting/SortingProblems.java
@@ -6,16 +6,24 @@ import java.util.HashMap;
 public class SortingProblems {
     public static void main(String[] args) {
 
+        int [] salaries = {4000,3000,1000,2000};
+        double result1 = average(salaries);
+        System.out.println(result1);
+
 //        int [] A = {3, 2, 1, 3};
 //        int [] A = {1, 1, 3, 3};
 //        int [] A = {0, 1, 2, 0, 1, 2};
 //        int [] A = {0};
-        int firstArray [] = {3,7,8,9,10,1,2,3};
+        int firstArray [] = {8, 16, 80, 55, 32, 8, 38, 40, 65, 18, 15, 45, 50, 38, 54, 52, 23, 74, 81, 42, 28, 16, 66, 35, 91, 36, 44, 9, 85, 58, 59, 49, 75, 20, 87, 60, 17, 11, 39, 62, 20, 17, 46, 26, 81, 92};
+
+        System.out.println(Arrays.toString(mergeSort(firstArray)));
+//                {3,7,8,9,10,1,2,3};
+
         int secondArray [] = {2,5,6,11,12};
 //        int finalArray [] = mergeSortedArrays(firstArray,secondArray);
 //        System.out.println(Arrays.toString(finalArray));
 
-        System.out.println(Arrays.toString(merge(firstArray,0,firstArray.length/2,firstArray.length-1)));
+//        System.out.println(Arrays.toString(merge(firstArray,0,firstArray.length/2,firstArray.length-1)));
         int [] A = {2, 0, 0, 1, 0, 0, 2, 2, 1, 1, 0, 0, 1, 0, 2, 1, 1, 0, 1, 0, 1, 2, 2, 2, 0, 0, 1, 0, 2, 1, 1, 2, 1, 2, 2, 1, 0, 2, 2, 1, 1, 1, 0, 1, 0, 1, 0, 2, 1, 2, 0, 2, 0, 1, 1, 0, 2, 2, 1, 2, 0, 2, 1, 1, 1, 2, 0, 1, 0, 2, 2, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 2, 1, 1, 0, 2, 1, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 1, 1, 0, 2, 1, 2, 2, 2, 1, 2, 2, 0, 1, 0, 1, 2, 1, 1, 0, 1, 2, 0, 1, 0, 2, 2, 1, 2, 1, 0, 2, 2, 1, 1, 0, 2, 1, 2};
         int [] answerArray = sortColors(A);
         System.out.println(Arrays.toString(answerArray));
@@ -24,35 +32,65 @@ public class SortingProblems {
         System.out.println(result);
     }
 
-    static int [] merge(int [] A, int s, int m,int e){
+    static double average(int[] salary) {
 
-        int [] finalArray = new int[e-s+1];
+        Arrays.sort(salary);
 
-        int p1 = s; int p2 = m+1; int p3 = 0;
+        int sum = 0;
 
-        while (s <= m && p2 <= e){
-            if (A[p1] < A[p2]){
-                finalArray[p3] = A[p1];
+        for(int i = 1; i < salary.length-1;i++){
+            sum = sum + salary[i];
+        }
+
+        int lengthOfArray = salary.length-2;
+
+        double average = sum/lengthOfArray;
+
+        return average;
+
+    }
+
+    static int [] merge(int [] first, int [] second){
+
+        int [] finalArray = new int[first.length + second.length];
+
+        int p1 = 0; int p2 = 0; int p3 = 0;
+
+        while (p1 < first.length && p2 < second.length){
+            if (first[p1] < second[p2]){
+                finalArray[p3] = first[p1];
                 p1++;
                 p3++;
             }else {
-                finalArray[p3] = A[p2];
+                finalArray[p3] = second[p2];
                 p2++;
                 p3++;
             }
         }
-        while (p1 <= m){
-            finalArray[p3] = A[p1];
+        while (p1 < first.length){
+            finalArray[p3] = first[p1];
             p1++;
             p3++;
         }
-        while (p2 <= e){
-            finalArray[p3] = A[p2];
+        while (p2 < second.length){
+            finalArray[p3] = second[p2];
             p2++;
             p3++;
         }
 
         return finalArray;
+    }
+
+    static int [] mergeSort(int [] A){
+
+        if (A.length == 1) return A;
+
+        int middle = (A.length) /2;
+
+        int left [] = mergeSort(Arrays.copyOfRange(A,0,middle));
+        int right [] = mergeSort(Arrays.copyOfRange(A,middle,A.length));
+
+        return merge(left,right);
     }
 
     static int [] mergeSortedArrays(int [] A, int [] B){
@@ -114,6 +152,8 @@ public class SortingProblems {
                     count++;
                 }
             }
+
+
             if (count == A[i]){
                 System.out.println(A[i] + " is a Noble Integer");
                 return 1;
