7a06623d1f9a8798e87f78cfbae343063e83dec3
Pradeep_Reddy_Kypa <deepu462@gmail.com>
2023-03-16 22:35:40 +0530

    Solution to sort Array using Quick Sort

--- src/Sorting/SortingProblems.java
+++ src/Sorting/SortingProblems.java
@@ -8,7 +8,14 @@ public class SortingProblems {
     static int count = 0;
     public static void main(String[] args) {
 
-        int [] array = {45, 10, 15, 25, 50};
+//        int [] array = {45, 10, 15, 25, 50};
+        int [] array = {5, 3, 1, 9, 4};
+        quickSort(array,0,array.length-1);
+        System.out.println(Arrays.toString(array));
+
+        int count = insertionSort(array);
+        System.out.println(count);
+
         mergeSortInPlace(array,0,array.length-1);
 
         System.out.println("The total count is - " + count);
@@ -37,7 +44,60 @@ public class SortingProblems {
         System.out.println(Arrays.toString(answerArray));
 //        int [] A = {-4, 7, 5, 3, 5, -4, 2, -1, -9, -8, -3, 0, 9, -7, -4, -10, -4, 2, 6, 1, -2, -3, -1, -8, 0, -8, -7, -3, 5, -1, -8, -8, 8, -1, -3, 3, 6, 1, -8, -1, 3, -9, 9, -6, 7, 8, -6, 5, 0, 3, -4, 1, -10, 6, 3, -8, 0, 6, -9, -5, -5, -6, -3, 6, -5, -4, -1, 3, 7, -6, 5, -8, -5, 4, -3, 4, -6, -7, 0, -3, -2, 6, 8, -2, -6, -7, 1, 4, 9, 2, -10, 6, -2, 9, 2, -4, -4, 4, 9, 5, 0, 4, 8, -3, -9, 7, -8, 7, 2, 2, 6, -9, -10, -4, -9, -5, -1, -6, 9, -10, -1, 1, 7, 7, 1, -9, 5, -1, -3, -3, 6, 7, 3, -4, -5, -4, -7, 9, -6, -2, 1, 2, -1, -7, 9, 0, -2, -2, 5, -10, -1, 6, -7, 8, -5, -4, 1, -9, 5, 9, -2, -6, -2, -9, 0, 3, -10, 4, -6, -6, 4, -3, 6, -7, 1, -3, -5, 9, 6, 2, 1, 7, -2, 5};
         int result = nobleInteger(A);
+
         System.out.println(result);
+    }
+
+    static void quickSort(int [] array, int start, int end){
+
+        if (start >= end) return;
+
+        int p = rearrange(array,start,end);
+        quickSort(array,start,p-1);
+        quickSort(array,p+1,end);
+    }
+
+    static int rearrange(int [] array, int start, int end){
+        int p1 = start+1; int p2 = end;
+
+        while (p1 <= p2){
+            if (array[p1] <= array[start]){
+                p1++;
+            } else if (array[p2] > array[start]) {
+                p2--;
+            }else {
+                int temp = array[p1];
+                array[p1] = array[p2];
+                array[p2] = temp;
+                p1++;
+                p2--;
+            }
+        }
+
+        int temp = array[start];
+        array[start] = array[p2];
+        array[end] = temp;
+
+        return p2;
+    }
+
+    static int insertionSort(int [] A){
+        int count = 0;
+        for(int i = 1; i < A.length;i++){
+            for(int j = i-1; j >= 0; j--){
+                if(A[j] > A[j+1]){
+                    count++;
+                    int temp = A[j];
+                    A[j] = A[j+1];
+                    A[j+1] = temp;
+                    int ans = A[i] * (1 << i);
+                    System.out.println("Count is " + count + " when i is " + i + " " + Arrays.toString(A));
+                }else{
+                    break;
+                }
+            }
+        }
+        return count;
     }
 
     static double average(int[] salary) {
